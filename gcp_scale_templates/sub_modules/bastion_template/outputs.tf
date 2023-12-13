8f676f5e909b48c872b8e4df0aaa0d165b4c5418
Sasikanth Eda <sasikeda@users.noreply.github.com>
2023-12-13 20:33:22 +0530

    Remove bastion instance outputs, private only, allow packer os login input (#457)
    * Attempt to seperate out packer login username
    
    * Avoid NAT in private only networks
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Replace deprecated account_file with credentials_json
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Fix the packer syntax issues
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Modify the vpc_id to vpc_ref
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Fix typo of ssh username
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Remove the bastion instance fetch calls and outputs
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    ---------
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- gcp_scale_templates/sub_modules/bastion_template/outputs.tf
+++ gcp_scale_templates/sub_modules/bastion_template/outputs.tf
@@ -1,9 +1,10 @@
-output "bastion_firewall_id" {
-  value       = module.bastion_firewall.firewall_id
-  description = "Bastion firewall id."
+output "bastion_instance_autoscaling_group_ref" {
+  value       = module.bastion_autoscaling_group.instance_group_manager_ref
+  description = "Bastion instances autoscaling group (id/self-link)."
 }
 
-output "bastion_firewall_name" {
-  value       = module.bastion_firewall.firewall_name
-  description = "Bastion firewall name."
+output "bastion_security_group_ref" {
+  value       = local.bastion_network_tag
+  depends_on  = [module.allow_traffic_from_external_cidr_to_bastion]
+  description = "Bastion firewall id."
 }
