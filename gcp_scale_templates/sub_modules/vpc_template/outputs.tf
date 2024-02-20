c9a9c4e5c862cc6c954cc3d93d7cce1bc803880a
Sasikanth Eda <sasikeda@users.noreply.github.com>
2024-02-20 16:02:28 +0530

    Include protocol rpm installation to image (#480)
    * Include protocol rpm installation to image
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Fix the gcp vpc and aws packer
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Update the gcp variables inline to aws
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Fix the encryption key interpolation
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    ---------
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- gcp_scale_templates/sub_modules/vpc_template/outputs.tf
+++ gcp_scale_templates/sub_modules/vpc_template/outputs.tf
@@ -1,34 +1,29 @@
-output "vpc_name" {
-  value       = module.vpc.vpc_name
+output "vpc_ref" {
+  value       = module.vpc.vpc_self_link
   description = "VPC name."
 }
 
-output "vpc_id" {
-  value       = module.vpc.vpc_id
-  description = "The ID of the VPC."
-}
-
 output "vpc_public_subnets" {
-  value       = module.public_subnet.subnet_id
+  value       = module.public_subnet.subnet_uri
   description = "List of IDs of public subnets."
 }
 
 output "vpc_storage_cluster_private_subnets" {
-  value       = module.storage_private_subnet.subnet_id
+  value       = module.storage_private_subnet.subnet_uri
   description = "List of IDs of storage cluster private subnets."
 }
 
 output "vpc_compute_cluster_private_subnets" {
-  value       = module.compute_private_subnet.subnet_id
+  value       = module.compute_private_subnet.subnet_uri
   description = "List of IDs of compute cluster private subnets."
 }
 
-output "vpc_compute_cluster_nat" {
+output "vpc_compute_nat_gateways" {
   value       = module.compute_cloud_nat.cloud_nat_id
-  description = "List of IDs of compute cluster nat."
+  description = "List of IDs of compute cluster nat gateway."
 }
 
-output "vpc_storage_cluster_nat" {
+output "vpc_storage_nat_gateways" {
   value       = module.storage_cloud_nat.cloud_nat_id
-  description = "List of IDs of storage cluster nat."
+  description = "List of IDs of storage cluster nat gateway."
 }
